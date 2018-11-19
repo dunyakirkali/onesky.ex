8e9c6e950fc9a81ac764cd8ad40f39fc8029cfdd
Dunya Kirkali <dunyakirkali@gmail.com>
2018-11-19 22:04:20 +0100

    Initial commit

---
+++ lib/onesky.ex
@@ -0,0 +1,18 @@
+defmodule Onesky do
+  @moduledoc """
+  Documentation for Onesky.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Onesky.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/onesky_test.exs
@@ -0,0 +1,8 @@
+defmodule OneskyTest do
+  use ExUnit.Case
+  doctest Onesky
+
+  test "greets the world" do
+    assert Onesky.hello() == :world
+  end
+end
