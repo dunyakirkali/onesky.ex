09176cb118fce56dae3672246b27c2ca7ccd5e0c
Dunya Kirkali <dunyakirkali@gmail.com>
2018-11-21 21:50:22 +0100

    Test data length or locales

--- test/onesky/resources/locale_test.exs
+++ test/onesky/resources/locale_test.exs
@@ -9,8 +9,11 @@ defmodule LocaleTest do
       {:ok, %Tesla.Env{} = env} = Onesky.client() |> Onesky.Locale.list_locales()
 
       assert env.status == 200
+
       assert env.body["meta"]["status"] == 200
       assert env.body["meta"]["record_count"] == 487
+
+      assert length(env.body["data"]) == 487
     end
   end
 end
