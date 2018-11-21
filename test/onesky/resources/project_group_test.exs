2c4d69bcc328868c899bb4c9a413f3cfb220798b
Dunya Kirkali <dunyakirkali@gmail.com>
2018-11-21 21:50:07 +0100

    Use var for project_group tests

--- test/onesky/resources/project_group_test.exs
+++ test/onesky/resources/project_group_test.exs
@@ -1,20 +1,32 @@
 defmodule ProjectGroupTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+
   doctest Onesky.ProjectGroup
 
   test "list_project_groups" do
-    {:ok, %Tesla.Env{} = env} = Onesky.client() |> Onesky.ProjectGroup.list_project_groups()
+    use_cassette "project_group#list" do
+      {:ok, %Tesla.Env{} = env} = Onesky.client() |> Onesky.ProjectGroup.list_project_groups()
 
-    assert env.status == 200
-    assert env.body["meta"]["status"] == 200
-    assert env.body["meta"]["record_count"] == 5
+      assert env.status == 200
+
+      assert env.body["meta"]["status"] == 200
+      assert env.body["meta"]["record_count"] == 5
+
+      assert length(env.body["data"]) == 5
+    end
   end
 
   test "languages" do
-    {:ok, %Tesla.Env{} = env} = Onesky.client() |> Onesky.ProjectGroup.languages(142066)
+    use_cassette "project_group#languages" do
+      {:ok, %Tesla.Env{} = env} = Onesky.client() |> Onesky.ProjectGroup.languages(142066)
 
-    assert env.status == 200
-    assert env.body["meta"]["status"] == 200
-    assert env.body["meta"]["record_count"] == 5
+      assert env.status == 200
+
+      assert env.body["meta"]["status"] == 200
+      assert env.body["meta"]["record_count"] == 5
+
+      assert length(env.body["data"]) == 5
+    end
   end
 end
