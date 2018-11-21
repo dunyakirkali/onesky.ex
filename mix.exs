8e94958f6192b374d0ee358e515ce412127c86fc
Dunya Kirkali <dunyakirkali@gmail.com>
2018-11-21 23:29:46 +0100

    Don’t support 1.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Onesky.MixProject do
     [
       app: :onesky,
       version: "0.1.0",
-      elixir: "~> 1.3",
+      elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
