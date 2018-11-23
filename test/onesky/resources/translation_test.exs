052cddfce8abf3496a07f2393fb4a8cf2047c530
Dunya Kirkali <dunyakirkali@gmail.com>
2018-11-23 10:44:43 +0100

    Add translation#get_translation_status

---
+++ test/onesky/resources/translation_test.exs
@@ -0,0 +1,24 @@
+defmodule TranslationTest do
+  use ExUnit.Case, async: true
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+
+  alias Tesla.Multipart
+
+  doctest Onesky.Translation
+
+  test "get_translation_status" do
+    use_cassette "translation#status" do
+      file = %{"file_name" => "Localizable.strings", "locale" => "nl-NL"}
+      {:ok, %Tesla.Env{} = env} = Onesky.client() |> Onesky.Translation.get_translation_status(314254, file)
+
+      assert env.status == 200
+
+      assert env.body["meta"]["status"] == 200
+
+      assert env.body["data"]["file_name"] == "Localizable.strings"
+      assert env.body["data"]["progress"] == "100.0%"
+      assert env.body["data"]["string_count"] == 534
+      assert env.body["data"]["word_count"] == 2478
+    end
+  end
+end
