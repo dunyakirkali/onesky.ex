a737c63950a71d61d5e303ed80c4edc1be284524
Dunya Kirkali <dunyakirkali@gmail.com>
2019-12-31 16:39:25 +0100

    Implement SHOW quotations

---
+++ test/onesky/resources/quotation_test.exs
@@ -0,0 +1,19 @@
+defmodule QuotationTest do
+  use ExUnit.Case, async: true
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+
+  doctest Onesky.Quotation
+
+  test "show_quotation" do
+    use_cassette "quotation#show" do
+      {:ok, %Tesla.Env{} = env} = Onesky.client() |> Onesky.Quotation.show_quotation(314254)
+
+      assert env.status == 200
+
+      assert env.body["meta"]["status"] == 200
+
+      assert env.body["data"]["specialization"] == "general"
+      assert env.body["data"]["is_including_not_approved"] == true
+    end
+  end
+end
