405d16fbf220e2c78eb0223f82d74b219c811840
Dunya Kirkali <dunyakirkali@gmail.com>
2020-01-01 15:03:00 +0100

    Bump version to 0.16.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Onesky.MixProject do
   def project do
     [
       app: :onesky,
-      version: "0.16.0",
+      version: "0.16.1",
       elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
